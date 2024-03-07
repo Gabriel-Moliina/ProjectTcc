df08fc25a9573cd72e5b477657004bd73c089ea4
Molina <biel_molina9@hotmail.com>
2024-03-07 20:39:20 -0300

    centralizando div

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,4 +3,8 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [{
     path: 'login',
     loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)
-  }];
+  },{
+    path: '',
+    redirectTo: 'login', pathMatch: 'full'
+  }
+];
