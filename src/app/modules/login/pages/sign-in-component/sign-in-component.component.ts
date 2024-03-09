0786b252c69eb042210b2e3c9e3562d144715a9d
Molina <biel_molina9@hotmail.com>
2024-03-09 12:14:45 -0300

    Criando componente tela de login

--- src/app/modules/login/pages/sign-in-component/sign-in-component.component.ts
+++ src/app/modules/login/pages/sign-in-component/sign-in-component.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { InputComponent } from "../../../../view/input/input.component";
 
 @Component({
-  selector: 'app-sign-in-component',
-  standalone: true,
-  imports: [RouterLink],
-  templateUrl: './sign-in-component.component.html',
-  styleUrl: './sign-in-component.component.css'
+    selector: 'app-sign-in-component',
+    standalone: true,
+    templateUrl: './sign-in-component.component.html',
+    styleUrl: './sign-in-component.component.css',
+    imports: [RouterLink, InputComponent]
 })
 export class SignInComponent {
 
