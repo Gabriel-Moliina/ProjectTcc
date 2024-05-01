bebbd0d48e0ead8e7ba7eeadb5284eb6f4dcbb75
Molina <biel_molina9@hotmail.com>
2024-05-01 16:38:33 -0300

    Inserindo grid no container principal

--- src/app/features/tcc/pages/project/project.component.ts
+++ src/app/features/tcc/pages/project/project.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatGridListModule],
   templateUrl: './project.component.html',
   styleUrl: './project.component.css'
 })
--- src/app/features/tcc/tcc.component.ts
+++ src/app/features/tcc/tcc.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { MenuComponent } from "./shared/menu/menu.component";
+import { ProjectComponent } from "./pages/project/project.component";
 
 @Component({
     selector: 'app-tcc',
     standalone: true,
     templateUrl: './tcc.component.html',
     styleUrl: './tcc.component.css',
-    imports: [MatSidenavModule, MenuComponent]
+    imports: [MatSidenavModule, MenuComponent, ProjectComponent]
 })
 export class TccComponent {
 
