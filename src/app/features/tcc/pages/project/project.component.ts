668c9fccf3eb753852da4a6bb63dd73b768410eb
Molina <biel_molina9@hotmail.com>
2024-05-07 12:42:13 -0300

    Iniciando tela de projeto

--- src/app/features/tcc/pages/project/project.component.ts
+++ src/app/features/tcc/pages/project/project.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import {MatGridListModule} from '@angular/material/grid-list';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [RouterLink, MatGridListModule],
+  imports: [CommonModule, RouterLink, MatGridListModule],
   templateUrl: './project.component.html',
   styleUrl: './project.component.css'
 })
-export class ProjectComponent {
 
+export class ProjectComponent {
+  hasProject:boolean = true
 }
