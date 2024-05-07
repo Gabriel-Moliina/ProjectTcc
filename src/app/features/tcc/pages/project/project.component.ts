8f1d9624f738e54b81433b9e20d00a9b958cb4bf
Molina <biel_molina9@hotmail.com>
2024-05-07 20:28:59 -0300

    Implementando tela para novo cadastro de projeto

--- src/app/features/tcc/pages/project/project.component.ts
+++ src/app/features/tcc/pages/project/project.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '../../../../core/components/button/button.component';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [CommonModule, RouterLink, MatGridListModule],
+  imports: [CommonModule, RouterLink, MatGridListModule, ButtonComponent],
   templateUrl: './project.component.html',
   styleUrl: './project.component.css'
 })
 
 export class ProjectComponent {
-  hasProject:boolean = true
+  constructor(private router: Router) {
+        
+  }
+  
+  createProject() {
+      this.router.navigate(['/tcc/project/create']);
+  }
+  hasProject:boolean = false
 }
